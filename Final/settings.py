280a488a7457cc92b3a063644298d0e060b2e0ac
omolaylay <mersachay@gmail.com>
2023-06-01 15:44:21 +0100

    plsss

--- Final/settings.py
+++ Final/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["firstdepl.herokuapp.com/"]
+ALLOWED_HOSTS = ["firstdepl.herokuapp.com"]
 
 
 # Application definition
